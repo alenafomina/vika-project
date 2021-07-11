614f1da51be9357ad9cc5c41a771d83d15a5ec15
AlenaFomina <fominaizgagina@gmail.com>
2021-07-11 17:06:51 +0500

    version

--- script/script.js
+++ script/script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     $('.burger').click(function(event) {
         $('.burger, .nav').toggleClass('active');
     });
-    console.log(document.documentElement.clientHeight)
-    document.querySelector('.header').style.minHeight = `${document.documentElement.clientHeight}px`;
+    document.querySelector('.header').style.height = `${document.documentElement.clientHeight}px`;
 })
 
