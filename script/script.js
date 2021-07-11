6855a22ce1583919d51d61333c09cbf1f21d38f2
AlenaFomina <fominaizgagina@gmail.com>
2021-07-11 16:48:21 +0500

    height

--- script/script.js
+++ script/script.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     $('.burger').click(function(event) {
         $('.burger, .nav').toggleClass('active');
     });
+    document.querySelector('.header').style.minHeight = window.outerHeight;
 })
 
